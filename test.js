d991f3abad8a80bccfc6adf8e6b6521a085f9a27
huili.yang <huili.yang@qq.com>
2023-05-14 23:03:05 +0800

    算法题

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-function convertTemperature (number, type) {
+function convertTemperature (temperature, type) {
   const typeMap = {
-    'C': (number) => number * 1.8 + 32,
-    'F': (number) => (number - 32) * 1.8
+    'C': (temperature) => temperature * 1.8 + 32,
+    'F': (temperature) => (temperature - 32) * 1.8
   }
 
-  if (!number || !type) {
+  if (!temperature || !type) {
     throw new Error(
       'Parameter error, please enter temperature and temperature units'
     );
@@ -15,7 +15,7 @@ function convertTemperature (number, type) {
     );
   }
 
-  const res = typeMap[type](number);
+  const res = typeMap[type](temperature);
   console.log(res);
   return res;
 
@@ -26,13 +26,13 @@ convertTemperature(100, 'C') // 输出: 212
 
 function groupArrayElements (array, n) {
   const len = array.length;
-  
+
   if (!len || n < 1 || n > len) {
     return;
   }
  
   let averageFlag = len % n; // 能否平均分
-  const minLength = Math.floor(len / n);// 最少的组有多少个
+  const minLength = Math.floor(len / n);// 最少的组能有多少个元素
 
   const res = [];
   while (n) {
@@ -50,5 +50,5 @@ function groupArrayElements (array, n) {
 
 groupArrayElements([1, 2, 3, 4, 5], 2) // 输出: [[1, 2, 3], [4, 5]]
 groupArrayElements([1, 2, 3, 4, 5, 6], 4) // 输出: [[1, 2], [3, 4], [5], [6]]
-groupArrayElements([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],11); // 输出: [[1, 2], [3, 4], [5], [6]]
+
 
