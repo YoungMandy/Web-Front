8726b083afabd8af9bc072d28127426108393b31
huili.yang <huili.yang@qq.com>
2024-03-05 11:46:31 +0800

    docs: 原型链研究

--- 设计模式/构造函数/index.js
+++ 设计模式/构造函数/index.js
@@ -7,6 +7,15 @@ function Person (name, age, job) {
     console.log(this.name);
   }
 
-  let person1 = new Person('Name', 38, 'http://example.com');
-  let person2 = new Person('Age', 40, 'http://example.com');
+  
 }
+let person1 = new Person('Name', 38, 'http://example.com');
+console.log(person1.__proto__ == Person.prototype);
+console.log(person1.__proto__.constructor)
+console.log(Person.prototype.constructor)
+console.log(Person.prototype)
+console.log(Person.__proto__)
+
+console.log(person1 instanceof Object)
+ debugger;
+ let person2 = new Person('Age', 40, 'http://example.com');
