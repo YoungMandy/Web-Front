b16ce76c0426630326bab616cdf9e1eb52aec5b0
huili.yang <huili.yang@qq.com>
2024-03-11 10:04:58 +0800

    docs: 引用类型值覆盖

--- JavaScript/原型与原型链/index.js
+++ JavaScript/原型与原型链/index.js
@@ -13,11 +13,25 @@ function SubType () {
 
 SubType.prototype = new SuperType();// 构造函数调用返回的对象[[Prototype]] 链接到SuperType.prototype上
 
-SubType.prototype.getSuperValue = function() {
+SubType.prototype.getSubValue = function() {
   return this.subproperty;
 }
 
-var instance = new SuperType();
-console.log(instance.getSuperValue());
+var instance = new SubType();
+instance.property = false;
+console.log('getSuperValue', instance.getSuperValue());
+
+console.log(instance instanceof SubType);
+console.log(instance instanceof SuperType);
+console.log(instance instanceof Object);
+
+console.log(SubType instanceof Function)
+console.log(Function instanceof Object);
+console.log(Function.isPrototypeOf(instance))
+console.log(Object.prototype.isPrototypeOf(instance))
+console.log(Function.prototype.isPrototypeOf(SubType));
 
 
+// 原型链可能带来的问题：
+// 1. 整个原型链上的非预期的引用类型的值覆盖
+// 2. 不能在构造函数中传递参数
