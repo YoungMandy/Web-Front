06055d58f96d216596b77131cd442eb341cef304
huili.yang <huili.yang@qq.com>
2024-03-31 22:48:24 +0800

    docs: 对象的键值遍历

--- JavaScript/有序的遍历对象的属性.js
+++ JavaScript/有序的遍历对象的属性.js
@@ -21,3 +21,12 @@ for (let i = 0; i < keys.length; i++) {
   const key = keys[i];
   console.log(`${key}:${obj[key]}`);
 }
+
+console.log('\n');
+
+// 使用 for...in 循环遍历对象的属性值
+for (let key in obj) {
+  if (obj.hasOwnProperty(key)) { // 这一步是为了确保只遍历对象自身的属性
+    console.log(key,obj[key]); // 注意这里使用的是 obj[key] 来访问属性值
+  }
+}
