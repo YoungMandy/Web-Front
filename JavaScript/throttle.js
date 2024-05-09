1df26df5cbecab52f8e5490ee2df056e564d5652
huili.yang <huili.yang@qq.com>
2024-05-09 17:14:16 +0800

    feat: 加分号

--- JavaScript/throttle.js
+++ JavaScript/throttle.js
@@ -24,3 +24,31 @@ throttledFunc.call(obj2, 'Hello', 'world');
 // var name = 123;
 
 // myFunc('Hello', 'world');
+
+// function throttle(fn, gap) {
+  
+//   let flag = false;
+//   return function(...args) {
+//     if (flag) {
+//        return;
+//     } 
+//     flag = true;
+   
+//     setTimeout(() => {
+//       fn.apply(this, args);
+//       flag = false;
+//     }, gap);
+//   }
+// }
+
+function throttle (fn, gap) {
+  let flag = false;
+  return function(...args) {
+    if (flag) return;
+
+    setTimeout(() => {
+      flag = false;
+      fn.apply(this, args);
+    },gap)
+  }
+}
---
+++ 算法/05.08.js
@@ -0,0 +1,46 @@
+/*
+ * @desc: 
+ * @param: 
+ * @return: 
+ * @Author: huili.local
+ */
+// 1000.32 => 1,000.32
+// 100.5 => 100.5
+function formatPrice (price) {
+  
+  const list = price.split(".");
+  const interPart = list[0];// 整数部分
+  let interList = interPart.split("");
+
+  let len = interList.length;
+  let str = '';
+  let count = 0;
+
+  while (len) {
+    let ch = interList.pop();// 从尾部拿数
+   
+    if (count == 3) {//之前已经有三个数了
+      str = ch + ',' + str ;
+      count = 0;
+    } else {
+      str = ch + str
+    }
+    
+    count++;
+    len--;
+   
+  }
+
+  let res = list.length > 1 ? str + '.' + list[1] : str;
+ 
+  console.log(res);
+  return res;
+
+}
+formatPrice('10000000.32');
+formatPrice('1000000.32');
+formatPrice('10000.32');
+formatPrice('1000.32');
+formatPrice('100.32');
+formatPrice('10.32');
+formatPrice('1.32');
