b975cc65d65f912e931d575b14d8993237bf0bdf
huili.yang <huili.yang@qq.com>
2024-05-27 21:41:48 +0800

    docs: 优化

--- JavaScript/iterable.js
+++ JavaScript/iterable.js
@@ -1,8 +1,7 @@
 Object.prototype[Symbol.iterator] = function*() {
-  console.log('keys数组', JSON.stringify(Object.keys(this)));
-  for (const key of Object.keys(this)) {
-    yield this[key] ? this[key]: undefined;
-  }
+  for (let key of Object.keys(this)){
+   yield this[key];
+ }
 };
 var [a, b] = { a: 1, c:9, d: 2,f:3 };
 console.log(a, b);
