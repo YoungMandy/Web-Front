1d8543cbd00dad42d50f44b6cea41648f1906f14
huili.yang <huili.yang@qq.com>
2024-08-01 06:52:01 +0800

    feat: this的研究

--- 算法/0731.js
+++ 算法/0731.js
@@ -4,15 +4,11 @@
  * @return: 
  * @Author: huili.local
  */
-const action = 'action';
-const obj = new MyObject('hello');
-obj.wait(10).run(action);// 输出`{name} do {action}`,等待10秒
-obj.prewait(1000).run(action);// 等待10秒，输出`{name} do {action}`
+
 
 function MyObject (name) {
   this.immediate = false;
   this.gap = 0;
-
   this.wait = function(gap) {
     this.gap = gap;
     this.immediate = true;
@@ -36,3 +32,79 @@ function MyObject (name) {
     return this;
   }
 }
+
+
+function MyObject2 (name) {
+
+  function prewait (time) {
+
+    return {
+      run: function(action) {
+        setTimeout(() => {
+          console.log(`${name} do ${action}`)
+        }, time)
+      }
+    }
+  }
+
+  function wait (time) {
+    // const time = gap;
+    return {
+      run: function(action) {
+        console.log(`${name} do ${action}`)
+        setTimeout(() => {
+
+        }, time)
+      }
+    }
+  }
+  return {
+    prewait,
+    wait
+  }
+}
+
+class CustomObject {
+  gap = 0;
+  immediate = false;
+
+  constructor (name) {
+    this.name = name;
+  }
+
+  wait (gap) {
+    this.gap = gap;
+
+    this.immediate = true;
+    return this;
+  }
+
+  prewait (gap) {
+    this.gap = gap;
+    this.immediate = false;
+    return this;
+  }
+
+  run (action) {
+    if (this.immediate) {
+      console.log(`${this.name} do ${action}`);
+      setTimeout(() => {
+        return this;
+      }, this.gap)
+    } else {
+      setTimeout(() => {
+        console.log(`${this.name} do ${action}`);
+        return this;
+      }, this.gap)
+    }
+    
+  }
+
+
+
+}
+
+const action = 'action';
+const obj = new CustomObject('hello');
+obj.wait(10).run('action1');// 输出`{name} do {action}`,等待10秒
+obj.prewait(8000).run('action2');// 等待10秒，输出`{name} do {action}`
