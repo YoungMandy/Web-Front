76a80e68340944b8cc7fb424ca1e56824a0f69ec
huili.yang <huili.yang@qq.com>
2024-08-13 14:38:35 +0800

    feat: 算法研究

---
+++ 算法/getRandomNum.js
@@ -0,0 +1,27 @@
+function getRandomNumber (min, max) {
+  const res = Math.floor(Math.random() * (max - min + 1) + min) ;
+  console.log(res);
+  return res;
+}
+
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+getRandomNumber(1, 10);
+
+
+
