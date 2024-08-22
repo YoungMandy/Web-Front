a37f32e79dc4ba889aff4af3cb3f6ecc30ec5d33
huili.yang <huili.yang@qq.com>
2024-08-23 07:42:38 +0800

    feat: 优化

--- 算法/temp/8.21.js
+++ 算法/temp/8.21.js
@@ -1,29 +1,29 @@
-function throttle (fn, gap) {
-  let prev = 0;
-  return function(...rest) {
+// function throttle (fn, gap) {
+//   let prev = 0;
+//   return function(...rest) {
 
-    const now = Date.now();
-    if (now - prev > gap) {
-      fn.apply(this, rest);
-      prev = now;
-    }
-
-
-  }
-}
+//     const now = Date.now();
+//     if (now - prev > gap) {
+//       fn.apply(this, rest);
+//       prev = now;
+//     }
 
 
-function test (num) {
-  console.log(num);
-}
+//   }
+// }
 
-const a = throttle(test, 100);
-a(1);
 
-setTimeout(() => a(2), 50)
-setTimeout(() => a(3), 100)
-setTimeout(() => a(4), 100)
-setTimeout(() => a(5), 140)
-setTimeout(() => a(6), 210)
-setTimeout(() => a(7), 100)
+// function test (num) {
+//   console.log(num);
+// }
+
+// const a = throttle(test, 100);
+// a(1);
+
+// setTimeout(() => a(2), 50)
+// setTimeout(() => a(3), 100)
+// setTimeout(() => a(4), 100)
+// setTimeout(() => a(5), 140)
+// setTimeout(() => a(6), 210)
+// setTimeout(() => a(7), 100)
 
