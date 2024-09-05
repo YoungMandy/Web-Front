fd9c57ac4aa1a733faffa456a3c892b8cb26a6ab
huili.yang <huili.yang@qq.com>
2024-09-05 20:46:05 +0800

    feat: 算法

---
+++ 算法/0905.js
@@ -0,0 +1,14 @@
+const obj = {
+  num: 1,
+  text:'我劝天公重抖擞'
+}
+console.log(obj);
+obj.num++;
+obj.text = '不拘一格降人才';
+console.log(obj);
+
+const a = {
+  index:1
+}
+console.log(a);
+a.index++;
---
+++ 算法/huawei.js
@@ -0,0 +1,92 @@
+const root = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 4,
+      right: {
+        val: 8
+      }
+    },
+    right: {
+      val: 5
+    }
+  },
+  right: {
+    val: 3,
+    left: {
+      val: 6,
+      left: {
+        val: 9
+      },
+      right: {
+        val: 10
+      }
+    },
+    right: {
+      val: 7
+    }
+  }
+}
+
+//findPath（root，6）返回[1, 3, 6]
+
+function findPath (root, val) {
+  if (root == null) return;
+
+  let res = [];
+
+  function dfs (node, path) {
+    if (node == null) return;
+
+    path.push(node.val);
+
+    if (node.val == val) {
+      res = [...path];
+      return;
+    }
+
+    res.length < 1 && dfs(node.left,[...path]);
+    res.length < 1  && dfs(node.right,[...path]);
+  }
+
+  dfs(root, []);
+
+  console.log(res);
+
+  return res;
+}
+
+findPath(root, 6);
+findPath(root, 10);
+
+
+
+
+function add (...arg) {
+   let args = arg;
+
+  function a (...rest) {
+    
+    args = [...args, ...rest];
+
+    return a;
+  }
+
+
+  const b = {
+    toString : function() {
+
+      const sum = args.reduce((total, cur) => {
+        return total += cur;
+      }, 0)
+
+      return sum;
+    }
+  }
+
+  Object.setPrototypeOf(a, b);
+
+  return a;
+}
+console.log("sum=" + add(1)(2)(3).toString()); // sum=6
